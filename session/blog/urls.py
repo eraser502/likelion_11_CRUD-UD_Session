d24069e283435dda03f99d0fe0c8ce20611659eb
dohun <dohun@dohun-ui-MacBookPro.local>
2023-04-09 20:08:42 +0900

    first commit

--- session/blog/urls.py
+++ session/blog/urls.py
@@ -1,12 +1,14 @@
 from django.urls import path
 from . import views
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('', views.home, name='home'), # 127.0.0.1:8000
-    path('blog/<int:blog_id>', views.detail, name='detail'),
-    path('blog/new', views.new, name='new'),
-    path('blog/create', views.create, name='create'),
-    path('blog/edit/<int:blog_id>', views.edit, name='edit'),
-    path('blog/update/<int:blog_id>', views.update, name='update'),
-    path('blog/delete/<int:blog_id>', views.delete, name='delete')
-]
+    path('blog/<int:blog_id>/', views.detail, name='detail'),
+    path('blog/new/', views.new, name='new'),
+    path('blog/create/', views.create, name='create'),
+    path('blog/edit/<int:blog_id>/', views.edit, name='edit'),
+    path('blog/update/<int:blog_id>/', views.update, name='update'),
+    path('blog/delete/<int:blog_id>/', views.delete, name='delete'),
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
